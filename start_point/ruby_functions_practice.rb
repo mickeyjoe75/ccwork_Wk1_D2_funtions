0c5ae317018819961abd327d38fe235ebcd15df6
Joseph Manuel <mickeyjoe75@gmail.com>
2018-03-06 16:56:10 +0000

    end of the day of pair programming

---
+++ start_point/ruby_functions_practice.rb
@@ -0,0 +1,24 @@
+def return_10
+  return 10
+end
+
+def add(num1,num2)
+return num1 + num2
+end
+
+
+def subtract(num1,num2)
+return num1 - num2
+end
+
+def multiply(num1,num2)
+return num1 * num2
+end
+
+def divide(num1,num2)
+return num1 / num2
+end
+
+def length_of_string()
+ return "A string of length 21".length
+end
